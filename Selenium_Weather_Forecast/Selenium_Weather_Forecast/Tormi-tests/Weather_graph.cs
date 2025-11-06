b41ac923551eac77d85f6574c419e721cd88a968
tormipoiss <158473006+tormipoiss@users.noreply.github.com>
2025-11-06 18:20:18 +0200

    Added weather graph test

---
+++ Selenium_Weather_Forecast/Selenium_Weather_Forecast/Tormi-tests/Weather_graph.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Testing.Platform.Extensions.Messages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System.Xml.Linq;
+
+namespace Selenium_Weather_Forecast.Tormi_tests
+{
+    internal class Weather_graph
+    {
+        IWebDriver driver;
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+            driver.Url = "https://localhost:5001/";
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[contains(text(), 'Register') and contains(@class, 'btn-primary')]"))).Click();
+
+            string username = Guid.NewGuid().ToString();
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("Username"))).SendKeys(username);
+            driver.FindElement(By.Id("Password")).SendKeys("Abc-1");
+            driver.FindElement(By.Id("ConfirmPassword")).SendKeys("Abc-1");
+            driver.FindElement(By.XPath("//*[contains(@class, 'btn-primary')]")).Submit();
+
+            Assert.That(wait.Until(ExpectedConditions.TextToBePresentInElementLocated(By.CssSelector("h1"), "You have successfully registered your account, please log in with it")));
+
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.XPath("//*[contains(text(), 'Login') and contains(@class, 'nav-link')]"))).Click();
+            driver.FindElement(By.Id("Username")).SendKeys(username);
+            driver.FindElement(By.Id("Password")).SendKeys("Abc-1");
+            driver.FindElement(By.XPath("//*[contains(@class, 'btn-primary')]")).Submit();
+
+            Assert.That(wait.Until(ExpectedConditions.TextToBePresentInElementLocated(By.CssSelector("h1"), "Welcome to the weather forecast app")));
+        }
+        [Test]
+        public void Test()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+            driver.FindElement(By.Id("cityNameInput")).SendKeys("Tallinn");
+
+            driver.FindElement(By.XPath("//span[text()='Search specific day']")).Click();
+
+            Assert.That(wait.Until(ExpectedConditions.TextToBePresentInElementLocated(By.Id("weatherAddress"), "Tallinn")));
+
+            IWebElement weatherGraphButton = driver.FindElement(By.XPath("//span[text()='Show weather graph']"));
+
+            Actions actions = new Actions(driver);
+            actions.ScrollToElement(weatherGraphButton);
+            actions.Perform();
+
+            wait.Until(ExpectedConditions.ElementToBeClickable(weatherGraphButton)).Click();
+
+            Assert.That(wait.Until(ExpectedConditions.TextToBePresentInElementLocated(By.Id("meteogramFiller"), "Weather graph for: Tallinn")));
+
+            wait.Until(ExpectedConditions.ElementIsVisible(By.Id("meteogram")));
+        }
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Close();
+        }
+    }
+}
